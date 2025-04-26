502fcd4af2b009747802686c133d068bf8b49a76
mxkcpy <mikolaj19@poczta.fm>
2025-04-26 20:05:07 +0200

    refactored the test

--- test.cpp
+++ test.cpp
@@ -3,23 +3,27 @@
 #include <map>
 #include <stdexcept>
 #include <utility>
+// TODO:: refactor the getCharCodes to return a reversed map,
+// wrap the getCharCodes method in another method with the temp vector and map
+
 int main(int argc, char *argv[]) {
   if (argc != 2) {
     throw std::runtime_error("Please provide a valid file name");
   }
 
-  std::map<char, int> counted = Huffman::countOccurences(argv[1]);
-  const std::shared_ptr<HuffmanNode> tree = Huffman::createHuffmanTree(counted);
+  std::map<char, int> charOccurences = Huffman::countOccurences(argv[1]);
+  const std::shared_ptr<HuffmanNode> tree =
+      Huffman::createHuffmanTree(charOccurences);
 
   Huffman::printTree(tree);
 
-  std::map<std::string, char> map;
+  std::map<std::string, char> charCodes;
   std::vector<char> vec;
 
-  Huffman::getCharCodes(tree, vec, map);
+  Huffman::getCharCodes(tree, vec, charCodes);
 
   // Print the Huffman codes
-  for (std::pair<std::string, char> p : map) {
+  for (std::pair<std::string, char> p : charCodes) {
     std::cout << p.first << " lol: " << (int)p.second << " " << p.second
               << std::endl;
   }
