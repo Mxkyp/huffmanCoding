67455f97a615343029f59c4ab427b3b8ebbcec8c
mxkcpy <mikolaj19@poczta.fm>
2025-04-26 13:24:29 +0200

    added huffmanNodes, started writing an algo to create huffman tree

--- huffman.cpp
+++ huffman.cpp
@@ -1,5 +1,6 @@
 #include "huffman.hpp"
 #include <fstream>
+#include <vector>
 
 std::map<char, int> Huffman::countOccurences(const char *fileName) {
 
@@ -14,4 +15,13 @@ std::map<char, int> Huffman::countOccurences(const char *fileName) {
   fileFd.close();
 
   return occurences;
+}
+
+HuffmanNode createHuffmanTree(std::map<char, int> occurences) {
+  std::vector<HuffmanNode> trees;
+  trees.reserve(occurences.size());
+
+  for (auto &p : occurences) {
+    trees.push_back(HuffmanNode(p.first, p.second));
+  }
 }
--- huffman.hpp
+++ huffman.hpp
@@ -1,12 +1,29 @@
 #ifndef HUFFMAN_HPP
 #define HUFFMAN_HPP
 #include <map>
+#include <memory>
+
+struct HuffmanNode {
+public:
+  HuffmanNode(char character, int frequency)
+      : ch(character), freq(frequency), left(nullptr), right(nullptr) {};
+  void setLeft(HuffmanNode *ptr);
+  void setRight(HuffmanNode *ptr);
+
+private:
+  char ch;  // character, or '\0' if it's an internal node
+  int freq; // frequency
+  std::shared_ptr<HuffmanNode> left;
+  std::shared_ptr<HuffmanNode> right;
+};
 
 class Huffman {
 public:
   enum Mode { BINARY, TEXT };
   std::map<char, int> countOccurences(const char *fileName);
+  HuffmanNode createHuffmanTree(std::map<char, int> occurences);
 
 private:
 };
+
 #endif
